fdf0992dd9cc385186702e1ca50542ad0bde8151
Carl Lyons <code@carledwardlyons.ca>
2026-06-04 17:53:20 -0400

    Reverse Article Lists & Truncate Highlights
    When rendering article indexes, assume index file lists articles from
    oldest to newest and display them in reverse order. Allow landing page
    highlights to specify the max number of articles to display.

--- src/lib/utils/config.test.ts
+++ src/lib/utils/config.test.ts
@@ -120,7 +120,13 @@ describe('config highlights', () => {
       ...testConfig,
       highlights : [
         { id : 'art1', type : 'article', key : 'art1', section : 'Section 1' },
-        { id : 'tag1', type : 'tag', key : 'tag1', section : 'Section 1' },
+        {
+          id : 'tag1',
+          type : 'tag',
+          key : 'tag1',
+          count : 42,
+          section : 'Section 1',
+        },
         { id : 'tag2', key : 'tag2', section : 'Section 1' },
         { type : 'tag', key : 'tag1', section : 'Section 1' },
         { id : 'cat1', type : 'category', key : 'cat1' },
@@ -129,12 +135,19 @@ describe('config highlights', () => {
       ],
     });
     expect(config).toEqual(expect.objectContaining({ highlights : [
-      expect.objectContaining(
-        { id : 'art1', type : 'article', key : 'art1', section : 'Section 1' },
-      ),
-      expect.objectContaining(
-        { id : 'tag1', type : 'tag', key : 'tag1', section : 'Section 1' },
-      ),
+      expect.objectContaining({
+        id : 'art1',
+        type : 'article',
+        key : 'art1',
+        section : 'Section 1',
+      }),
+      expect.objectContaining({
+        id : 'tag1',
+        type : 'tag',
+        key : 'tag1',
+        count : 42,
+        section : 'Section 1',
+      }),
     ] }));
   });
 
@@ -152,6 +165,30 @@ describe('config highlights', () => {
       highlights : [
         expect.objectContaining({ id : 'tag1', key : 'tag1' }),
         expect.objectContaining({ id : 'tag2', key : 'tag2' }),
+      ],
+    }));
+  });
+
+  it('drops invalid highlight count', () => {
+    const config = buildConfig({
+      ...testConfig,
+      highlights : [{ id : 'tag1', type : 'tag', key : 'tag1', count : '42' }],
+    });
+    expect(config).toEqual(expect.objectContaining({
+      highlights : [
+        expect.objectContaining({ id : 'tag1', key : 'tag1', count : null }),
+      ],
+    }));
+  });
+
+  it('drops invalid highlight count', () => {
+    const config = buildConfig({
+      ...testConfig,
+      highlights : [{ id : 'tag1', type : 'tag', key : 'tag1', count : '42' }],
+    });
+    expect(config).toEqual(expect.objectContaining({
+      highlights : [
+        expect.objectContaining({ id : 'tag1', key : 'tag1', count : null }),
       ],
     }));
   });
